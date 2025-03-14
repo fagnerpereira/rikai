a8f4dcfbeaf6b11c94f5a0e14609c276724ed408
Fagner <fagner.pereira@prosas.com.br>
2025-03-14 00:09:46 -0300

    set test as pending

--- test/controllers/translations_controller_test.rb
+++ test/controllers/translations_controller_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class TranslationsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
+    skip "not implemented"
     get translations_index_url
     assert_response :success
   end
